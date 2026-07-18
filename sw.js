3170fc76f1d1ed40df00a3dae05dea0b556de7b2
juandiegocortazargonzalez5-collab <juandiegocortazargonzalez5@gmail.com>
2026-07-18 11:36:36 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mugs-sc-v1';
+const CACHE = 'mugs-sc-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
